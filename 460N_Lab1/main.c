d0f86cd83ba16ec3db134f2d813ac5f6846ba376
atchisonbrent <atchisonbrent@gmail.com>
2017-09-14 14:56:58 -0500

    Initial Commit

---
+++ 460N_Lab1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  460N_Lab1
+//
+//  Created by Brent Atchison on 9/14/17.
+//  Copyright © 2017 Brent Atchison. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

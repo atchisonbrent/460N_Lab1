68ff161be62cc6e02f8f352fdd1acbfebd81e33d
atchisonbrent <atchisonbrent@gmail.com>
2018-09-18 15:34:28 -0500

    Init

--- 460N_Lab1/assembler.c
+++ 460N_Lab1/assembler.c
@@ -1,6 +1,8 @@
 /*
  Name 1: Brent Atchison
+ Name 2: Kyle Garza
  UTEID 1: bma862
+ UTEID 2: k
 */
 
 #include <stdio.h>  /* standard input/output library */
@@ -8,18 +10,3 @@
 #include <string.h> /* String operations library */
 #include <ctype.h>  /* Library for useful character operations */
 #include <limits.h> /* Library for definitions of common variable type characteristics */
-
-int main(int argc, char* argv[]) {
-    
-    char *prgName   = NULL;
-    char *iFileName = NULL;
-    char *oFileName = NULL;
-    
-    prgName   = argv[0];
-    iFileName = argv[1];
-    oFileName = argv[2];
-    
-    printf("program name = '%s'\n", prgName);
-    printf("input file name = '%s'\n", iFileName);
-    printf("output file name = '%s'\n", oFileName);
-}
